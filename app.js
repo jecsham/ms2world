b98686c9947aa6b65aa8c143c6b7e33a8820a045
Jecsham <jecsham27@gmail.com>
2018-10-24 19:53:54 -0500

    adding some modules

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 const express = require('express');
+var session = require('express-session');
+const handlebars = require('express-handlebars');
+const handlebarshelpers = require('handlebars-helpers')();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+	limit: '50mb',
+	extended: false
+}));
+
+app.use(cookieParser());
+
+app.engine('handlebars', handlebars({
+	defaultLayout: 'main',
+	layoutsDir: `${__dirname}/views/layouts/`,
+	helpers: handlebarshelpers
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false
+}));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
