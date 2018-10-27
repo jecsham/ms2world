2ec8cf09697c99e79a8bb44e88ac98be2369c466
Jecsham <jecsham27@gmail.com>
2018-10-27 06:55:39 -0500

    adding support for custom static data

--- routes/constants.js
+++ routes/constants.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const gstatic = {};
 
 module.exports = {
-    gstatic: gstatic,
     router: router
 };
--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,5 @@
 const constants = require("./constants.js");
-var cstatic = '<link rel="stylesheet" href="/css/simplemde.css"> <script src="/js/simplemde.js"></script>';
-
+var cstatic =  ['<link rel="stylesheet" href="/css/simplemde.css">', '<script src="/js/simplemde.js"></script>'];
 constants.router.get('/test', (req, res) => {
     res.render('test', {
         gstatic: constants.gstatic,
