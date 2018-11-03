ba7c4eae6076c8ddfed2aa7ed68d93d44d9f7c08
Jecsham <jecsham27@gmail.com>
2018-11-03 06:17:11 -0500

    archer skillbuilder

--- public/js/global.js
+++ public/js/global.js
@@ -1,6 +1,12 @@
 // Header sticky
 $(document).ready(function () {
     $('#loginbtn').click( event => document.location.href = event.target.getAttribute('data-url'))
+    if(!Cookies.get('cookies-consent')){
+        $('#cookies-alert').addClass('show')
+        $('#cookies-alert').on('closed.bs.alert', () =>{
+            Cookies.set('cookies-consent', 'true', {expires: 365})
+        })
+    }
     jQuery("time.timeago").timeago();
     $("#header").sticky({
         topSpacing: 0,
