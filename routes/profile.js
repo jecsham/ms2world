db827a3d9bbac95f81f7876d783eb6cbb530269c
Jecsham <jecsham27@gmail.com>
2018-11-11 17:42:05 -0500

    profile imprevements

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,6 @@ module.exports = (app, constants) => {
         var sid = constants.sanitize(req.params.sid)
         constants.User_account.findOne({ sid: sid }, (err, account) => {
             if (err) return res.render('error')
-
             constants.Post_guide.paginate({ sid: sid }, { select: 'title author date_create voteCount viewCount', page: 1, limit: 10, sort: { voteCount: -1 } }, (err, guides) => {
                 if (err) return res.render('error')
                 constants.Post_build.paginate({ sid: sid }, { select: 'title author date_create voteCount viewCount', page: 1, limit: 10, sort: { voteCount: -1 } }, (err, builds) => {
@@ -13,6 +12,8 @@ module.exports = (app, constants) => {
                         title: 'Home - MS2World.com',
                         user: req.user,
                         account: account,
+                        guidesTotal: guides.totalDocs,
+                        buildsTotal: builds.totalDocs,
                         guides: guides.docs,
                         builds: builds.docs
                     });
